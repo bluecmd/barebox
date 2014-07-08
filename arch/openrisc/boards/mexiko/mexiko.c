c8875af310bd4b9f8ee47a4393060978e2e8ed68
Christian Svensson <blue@cmd.nu>
2014-07-09 01:29:17 +0200

    Added ethoc to Mexiko in Barebox

--- arch/openrisc/boards/mexiko/mexiko.c
+++ arch/openrisc/boards/mexiko/mexiko.c
@@ -18,6 +18,10 @@ static int openrisc_console_init(void)
 	add_ns16550_device(DEVICE_ID_DYNAMIC, OPENRISC_SOPC_UART_BASE, 1024,
 			   IORESOURCE_MEM | IORESOURCE_MEM_8BIT, &serial_plat);
 
+	add_generic_device("ethoc", DEVICE_ID_DYNAMIC, NULL,
+			   OPENRISC_SOPC_ETHOC_BASE, 0x1000,
+			   IORESOURCE_MEM, NULL);
+
 	return 0;
 }
 
